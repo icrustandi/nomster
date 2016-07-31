33b13db42fbea657dc0b9367d08f572300f12263
icrustandi <icrustandi@gmail.com>
2016-07-31 07:25:58 +0000

    Added create link in topnav

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all.page(params[:page]).per(1)
+    @places = Place.all.page(params[:page]).per(5)
   end
 
   def new
