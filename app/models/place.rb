3ee6f4db176909e17f33d4b1fdb81c733055a0b9
icrustandi <icrustandi@gmail.com>
2016-08-01 05:24:23 +0000

    User input validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ActiveRecord::Base
   belongs_to :user
+  validates :name, presence: true, length: { minimum: 3, too_short: "%{count} characters is the minimum allowed" }
+  validates :address, :description, presence:true
 end
