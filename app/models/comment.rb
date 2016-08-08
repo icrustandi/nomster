ed8055d1b6a611f589fff684ad8eed94e8ca8c4d
icrustandi <icrustandi@gmail.com>
2016-08-08 05:20:12 +0000

    Added mailer vis sendgrid

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
   validates :message, :rating, presence:true
+  after_create :send_comment_email
   RATINGS = {
     '✪✪✪✪✪'  => '5_stars',
     '✪✪✪✪'    => '4_stars',
@@ -16,4 +17,9 @@ class Comment < ActiveRecord::Base
   def humanized_rating
     RATINGS.invert[self.rating]
   end
+
+  def send_comment_email
+      NotificationMailer.comment_added(self).deliver
+  end
+
 end
