2c7966beb2aa2fc9a561b739c414ad3f3abe0865
icrustandi <icrustandi@gmail.com>
2016-08-17 04:40:50 +0000

    Fixed comments test

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,13 +3,19 @@ require 'rails_helper'
 RSpec.describe Place, type: :model do
 
   it "should delete its comments when deleted" do
-    @place = Place.create
-    @comment = Comment.create(place: @place)
+    @user = User.create!(email: "test@test.com", password: "password")
+    @place = Place.create!(user: @user, name: "pizza", address: "something st", description: "test desc") 
+    @rating = "2_stars"
+    @message= "test message"
+    @comment = Comment.create!(place: @place, rating: @rating, message: @message)
+    
     # When i call place.destroy the comment is also destroyed
     # expect{ @place.destroy }.to change(Comment, :count).by(-1)
-    
+    expect(Comment.where(id: @comment.id)).to_not be_empty
+    expect(Comment.count).to eq 1
     @place.destroy
     expect(Comment.where(id: @comment.id)).to be_empty
+    expect(Comment.count).to eq 0
   
   end
 end
